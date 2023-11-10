706680b7d3f572975a5a61d59506ac7a697c2b0a
GeeksforGeeks <GFGexample@gmail.orgg>
2023-11-10 13:40:33 +0600

    fontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { AiOutlineDelete } from 'react-icons/ai';
+import { BsCheck2Circle} from 'react-icons/bs';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] =useState (false);
+  const [allTodos,setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+
+const handleAddTodo = ()=>{
+  let newTodoItem = {
+    title:newTitle,
+    Description:newDescription
+  }
+
+  let updatedTodoArr = [ ...allTodos];
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My ToDos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="What's the task title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder="What's the task description?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button
+            className={`isCompleteScreen ${
+              isCompleteScreen === false && `active`
+            }`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            To-Do
+          </button>
+          <button
+            className={`isCompleteScreen ${
+              isCompleteScreen === true && `active`
+            }`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
+            Completed
+          </button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <div>
+            <h3>task 1</h3>
+            <p>Discription</p>
+            </div>
+            <div>
+              <AiOutlineDelete className="icon"/>
+              <BsCheck2Circle className="check-icon"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
